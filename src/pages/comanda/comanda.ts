42e2e3260a07aab6b2d6f93afb913c4403e779b2
Eduardo Nascimento de Aguiar <eduardo.x.aguiar@gmail.com>
2018-03-20 12:34:01 -0300

    ajustes no master

--- src/pages/comanda/comanda.ts
+++ src/pages/comanda/comanda.ts
@@ -6,50 +6,29 @@ import { NavController, NavParams } from 'ionic-angular';
   templateUrl: 'comanda.html'
 })
 export class ComandaPage {
-  selectedItem: any;
-  icons: string[];
-  items: Array<{title: string, note: string, icon: string}>;
-  consumos: Array<{id: string, checkin: {id: string},  produtosConsumo: Array<{produto:{id: string, descricao: string}, horaInclusao: string, valorAplicado: number, status: string }>}>;
+  produtosConsumo: Array<{ produto: { id: string, descricao: string }, dataHora: string, valorAplicado: number, status: string }>;
 
   constructor(public navCtrl: NavController, public navParams: NavParams) {
 
-    this.consumos = [];
-    this.consumos.push({
-      id: '019b5292-6d60-456e-82da-3debd5ecd84e', 
-      checkin: {
-        id:'1be72b33-be2c-4044-8a66-fd64f330bf8f'
-      }, 
-      produtosConsumo:[{
+    this.produtosConsumo = [];
+    this.produtosConsumo.push({
       produto: {
-        id:'asjpaçlsdçlasçld', 
-        descricao: 'teste 01' 
+        id: 'c82acfba-b4cb-4f73-bb15-9157d02688ba',
+        descricao: 'teste 01'
       },
-        horaInclusao: '00:30',
-        valorAplicado: 12.00,
-        status: 'atendido'}
-    ]});
+      dataHora: '00:30',
+      valorAplicado: 12.00,
+      status: 'atendido'
+    });
 
-    this.consumos.push({
-      id: 'asdasdasdas',
-      checkin: {
-        id: 'slkdalksdlajsdla'
+    this.produtosConsumo.push({
+      produto: {
+        id: 'e7c32b54-1553-4f79-8d6c-0e098ea4445a',
+        descricao: 'teste 02'
       },
-      produtosConsumo: [{
-        produto: {
-          id: 'asjpaçlsdçlasçld',
-          descricao: 'teste 01'
-        },
-        horaInclusao: '00:30',
-        valorAplicado: 12.00,
-        status: 'atendido'
-      }]
+      dataHora: '00:30',
+      valorAplicado: 12.00,
+      status: 'atendido'
     });
   }
-
-//   itemTapped(event, item) {
-//     // That's right, we're pushing to ourselves!
-//     this.navCtrl.push(ListPage, {
-//       item: item
-//     });
-//   }
 }
