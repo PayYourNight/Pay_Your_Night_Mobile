18b9ce9d851ba136dce4b86bbc69e6664c81b227
Eduardo Nascimento de Aguiar <eduardo.x.aguiar@gmail.com>
2018-03-23 16:07:59 -0300

    alterações pagina da comanda

--- src/pages/comanda/comanda.ts
+++ src/pages/comanda/comanda.ts
@@ -7,30 +7,93 @@ import { Platform } from 'ionic-angular';
   templateUrl: 'comanda.html'
 })
 export class TabComandaContentPage {
-  produtosConsumo: Array<{ produto: { id: string, descricao: string }, dataHora: string, valorAplicado: number, status: string }>;
-
+  // produtosConsumo: Array<{ produto: { id: string, descricao: string }, dataHora: string, valorAplicado: number, status: string }>;
+  pedidos: Array<{ id: string, dataHora: string, itensPedido: Array<{ id: string, quantidade: string, preco: string, unidade: string, produto: {id: string, descricao: string } }> }>;
   constructor(public navCtrl: NavController, public navParams: NavParams) {
-
-    this.produtosConsumo = [];
-    this.produtosConsumo.push({
-      produto: {
-        id: 'c82acfba-b4cb-4f73-bb15-9157d02688ba',
-        descricao: 'teste 01'
-      },
-      dataHora: '00:30',
-      valorAplicado: 12.00,
-      status: 'atendido'
+    this.pedidos = [];
+    this.pedidos.push({
+      id: 'c82acfba-b4cb-4f73-bb15-9157d02688ba',
+      dataHora: '22:00 AM',
+      itensPedido: [
+        {
+          id: 'e7c32b54-1553-4f79-8d6c-0e098ea4445a',
+          quantidade: '2',
+          preco: '12.00',
+          unidade: 'un',
+          produto: {
+            id: 'e7c32b54-1553-4f79-8d6c-0e098ea4445a',
+            descricao: 'teste 01' 
+          }
+        },
+        {
+          id: 'e7c32b54-1553-4f79-8d6c-0e098ea4445a',
+          quantidade: '2',
+          preco: '12.00',
+          unidade: 'un',
+          produto: {
+            id: 'e7c32b54-1553-4f79-8d6c-0e098ea4445a',
+            descricao: 'teste 01'
+          }
+        },
+        {
+          id: 'e7c32b54-1553-4f79-8d6c-0e098ea4445a',
+          quantidade: '2',
+          preco: '12.00',
+          unidade: 'un',
+          produto: {
+            id: 'e7c32b54-1553-4f79-8d6c-0e098ea4445a',
+            descricao: 'teste 01'
+          }
+        }
+      ],      
     });
 
-    this.produtosConsumo.push({
-      produto: {
-        id: 'e7c32b54-1553-4f79-8d6c-0e098ea4445a',
-        descricao: 'teste 02'
-      },
-      dataHora: '00:30',
-      valorAplicado: 12.00,
-      status: 'atendido'
+    this.pedidos.push({
+      id: 'c82acfba-b4cb - 4f73-bb15 - 9157d02688ba',
+      dataHora: '22:00 AM',
+      itensPedido: [
+        {
+          id: 'e7c32b54-1553-4f79-8d6c-0e098ea4445a',
+          quantidade: '2',
+          preco: '12.00',
+          unidade: 'un',
+          produto: {
+            id: 'e7c32b54-1553-4f79-8d6c-0e098ea4445a',
+            descricao: 'teste 01'
+          }
+        }
+      ],
     });
+    // this.produtosConsumo = [];
+    // this.produtosConsumo.push({
+    //   produto: {
+    //     id: 'c82acfba-b4cb-4f73-bb15-9157d02688ba',
+    //     descricao: 'teste 01'
+    //   },
+    //   dataHora: '00:30',
+    //   valorAplicado: 12.00,
+    //   status: 'atendido'
+    // });
+
+    // this.produtosConsumo.push({
+    //   produto: {
+    //     id: 'e7c32b54-1553-4f79-8d6c-0e098ea4445a',
+    //     descricao: 'teste 02'
+    //   },
+    //   dataHora: '00:30',
+    //   valorAplicado: 12.00,
+    //   status: 'atendido'
+    // });
+
+
+  }
+
+  goToProdutoDetail(produto: any){
+
+  }
+
+  doRefresh(event: any){
+
   }
 }
 
