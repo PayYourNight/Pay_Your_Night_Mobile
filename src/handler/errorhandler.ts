678c3cea98cdbc27c8fb33df885bb2aa51967a49
eduardo <eduardo.x.aguiar@gmail.com>
2018-07-12 19:45:58 -0300

    teste

--- src/handler/errorhandler.ts
+++ src/handler/errorhandler.ts
@@ -12,7 +12,7 @@ export class MyErrorHandler extends IonicErrorHandler implements ErrorHandler {
 
   handleError(err: any): void {
     console.log(err.status);
-    if (err.status != 412 && err.status != 404) {
+    if (err.status == 500) {
       console.log('Error: ' + err.message);
       Sentry.captureException(err);
       //super.handleError(err);
